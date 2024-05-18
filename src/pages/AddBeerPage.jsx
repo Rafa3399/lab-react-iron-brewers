8825c07400d0ed35a2b5553d223ce85b177efa52
Rafa <rafaokc33@gmail.com>
2024-05-18 15:51:49 +0200

    Lab Solved

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -27,14 +29,37 @@ function AddBeerPage() {
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  
+  const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    console.log("submitting...");
+    e.preventDefault();
+    const standardForm = {
+      name,
+      tagline,
+      description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: String(attenuationLevel),
+      contributed_by: contributedBy,
+    };
+    axios
+      .post(`https://ih-beers-api2.herokuapp.com/beers/new/new`, standardForm)
+      .then((response) => {
+        console.log(response);
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  };
 
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -122,7 +147,7 @@ function AddBeerPage() {
             value={contributedBy}
             onChange={handleContributedBy}
           />
-          <button className="btn btn-primary btn-round">Add Beer</button>
+          <button className="btn btn-primary btn-round" type="submit">Add Beer</button>
         </form>
       </div>
     </>
